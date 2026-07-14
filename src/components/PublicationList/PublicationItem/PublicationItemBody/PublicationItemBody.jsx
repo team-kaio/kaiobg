4e6816e21ce4f22dc417d3a536ff8c6665cbdc85
Nathalia Pissuti <nathaliapissuti@gmail.com>
2026-07-14 00:29:30 -0300

    Articles with html files

--- src/components/PublicationList/PublicationItem/PublicationItemBody/PublicationItemBody.jsx
+++ src/components/PublicationList/PublicationItem/PublicationItemBody/PublicationItemBody.jsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 
 import { ArticleContent } from '@/components/ArticleContent';
 import { FieldWithLabel, Input } from '@/components/Forms';
+import { SUPPORTED_LANGUAGES_ARRAY } from '@/constants';
 import { utils } from '@/utils';
 
 import styles from './PublicationItemBody.module.scss';
@@ -12,35 +13,44 @@ const PublicationItemBody = (props) => {
 
   const { t } = useTranslation();
 
-  const articlePath = item.content[selectedLanguage] || '';
-
-  const onUpdateArticlePath = useCallback((event) => {
-    const updatedContent = {
-      ...item.content,
-      [selectedLanguage]: event.target.value,
-    };
-
-    onUpdateItemProperty('content', updatedContent);
-  }, [ item.content, onUpdateItemProperty, selectedLanguage ]);
-
   const renderEditMode = useCallback(() => {
     return (
-      <FieldWithLabel
-        label={t('Article path')}
-        field={(
-          <Input
-            type="text"
-            name="articlePath"
-            value={articlePath}
-            placeholder="/articles/exemplo.html"
-            onChange={onUpdateArticlePath}
-          />
-        )}
-      />
+      <div className={styles.allLanguagesContainer}>
+        {SUPPORTED_LANGUAGES_ARRAY.map(language => {
+          const articlePath = item.content[language.code] || '';
+
+          const onUpdateArticlePath = (event) => {
+            const updatedContent = {
+              ...item.content,
+              [language.code]: event.target.value,
+            };
+
+            onUpdateItemProperty('content', updatedContent);
+          };
+
+          return (
+            <FieldWithLabel
+              key={language.code}
+              label={`${t('Article path')} (${language.name})`}
+              field={(
+                <Input
+                  type="text"
+                  name={`articlePath-${language.code}`}
+                  value={articlePath}
+                  placeholder={`/articles/exemplo-${language.code}.html`}
+                  onChange={onUpdateArticlePath}
+                />
+              )}
+            />
+          );
+        })}
+      </div>
     );
-  }, [ articlePath, onUpdateArticlePath, t ]);
+  }, [ item.content, onUpdateItemProperty, t ]);
 
   const renderViewMode = useCallback(() => {
+    const articlePath = item.content[selectedLanguage] || '';
+
     if(!articlePath) {
       return t('<empty>');
     }
@@ -54,7 +64,7 @@ const PublicationItemBody = (props) => {
         />
       </div>
     );
-  }, [ articlePath, t ]);
+  }, [ item.content, selectedLanguage, t ]);
 
   return (
     <div className={styles.PublicationItemBody}>
