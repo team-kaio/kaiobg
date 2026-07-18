b406195bc750bb358407fecbc1b0fdf920a9bd9d
Nathalia Pissuti <nathaliapissuti@gmail.com>
2026-07-18 13:27:38 -0300

    test: Test

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { store } from '@/store';
 import { App } from './App.jsx';
 import './i18n';
 
+console.log('TESTE');
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
